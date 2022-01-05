a5db1f814af06ee5522885a069a45d6cf50aa318
Alex Borges <alexborgessjc@hotmail.com>
2022-01-05 14:57:04 -0300

    Envio do exercício 4

---
+++ sem8_exercicio4/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Button from './components/button';
+
+function App() {
+  return (
+    <Button title="Get Started" background="#52a2f5"/>
+  );
+}
+
+export default App;
---
+++ sem8_exercicio4/src/components/button.js
@@ -0,0 +1,7 @@
+function Button({title,background}){
+    return(
+        <button style={{background:background}}>{title}</button>
+    )
+}
+
+export default Button
